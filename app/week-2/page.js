b7a06d979c5dc83cfe4413b5ed9ddc61a5188836
zeynep-ozdemir1905 <barikazeynep2@gmail.com>
2025-09-17 11:41:56 -0600

    lab done

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
+
+
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Github Username: zeynep-ozdemir1905</p>
+      <p>GitHub Repository: <Link href="https://github.com/zeynep-ozdemir1905/cprg306-assignments">zeynep-ozdemir1905/cprg306-assignments</Link></p>
+    </div>
+  );
+}

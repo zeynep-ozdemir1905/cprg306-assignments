fa824881281cac25bdf8bb414be6fbc431c87dc4
zeynep-ozdemir1905 <barikazeynep2@gmail.com>
2025-10-22 12:24:13 -0600

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,18 +1,10 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="p-3 border rounded-md shadow-sm bg-white flex justify-between">
-      <span className="font-semibold">{name}</span>
-      <span className="text-gray-600">Qty: {quantity}</span>
-      <span className="italic text-sm text-blue-500">{category}</span>
-    </li>
-  );
-}
-
     <li className="p-3 border rounded-md shadow-sm bg-gradient-to-r from-blue-50 to-blue-100 flex items-center justify-between">
-      {/* prodouct name */}
+      {/* product name */}
       <span className="font-semibold w-1/3">{name}</span>
 
-      {/* quantity*/}
+      {/* quantity */}
       <span className="text-gray-700 text-center w-1/3">Qty: {quantity}</span>
 
       {/* category */}
@@ -20,5 +12,5 @@ export default function Item({ name, quantity, category }) {
         {category}
       </span>
     </li>
- );
+  );
 }
